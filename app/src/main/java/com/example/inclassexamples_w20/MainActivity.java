9d994be72f088de3060c2959028221767e434544
Eric Torunski <etorunski@gmail.com>
2020-01-10 19:03:16 -0500

    looking at layouts and widgets in week 2

--- app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
@@ -9,6 +9,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // setContentView loads objects onto the screen.
+        // Before this function, the screen is empty.
         setContentView(R.layout.activity_main);
+
+        //Now that the screen was loaded, use findViewByid() to
+        // get load the objects in Java:
+
     }
 }
