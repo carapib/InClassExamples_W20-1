adeb21cf87080f02be4d6ef6e332ebc535a472a6
Eric Torunski <etorunski@gmail.com>
2020-01-16 10:34:48 -0500

    at slide  26

--- app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.inclassexamples_w20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
         //Now that the screen was loaded, use findViewByid() to
         // get load the objects in Java:
+        TextView firstText = findViewById(R.id.firstText);
 
+        EditText theEdit = findViewById(R.id.firstEditText);
+
+        String message = getResources().getString(R.string.hello);
+        theEdit.setText(message);
+
+        final Button btn = findViewById(R.id.myButton);
+        btn.setText("New strings");
+        btn.setOnClickListener( (click) ->  { btn.setText("You clicked me"); }  );
+
+
+        CheckBox cb = findViewById(R.id.checkb);
+        cb.setOnCheckedChangeListener( (compoundButton, b) -> {
+
+            Toast.makeText(MainActivity.this, "Checkbox is " + b, Toast.LENGTH_LONG).show();
+
+        });
     }
 }
