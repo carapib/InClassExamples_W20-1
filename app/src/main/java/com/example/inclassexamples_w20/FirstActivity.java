f0ec8cd87fb9664d486b99ccac548e50711a36e8
Eric Torunski <etorunski@gmail.com>
2020-01-23 11:58:52 -0500

    week 4 examples

--- app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
@@ -1,16 +1,11 @@
 package com.example.inclassexamples_w20;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 
 public class FirstActivity extends AppCompatActivity {
 
-    SharedPreferences prefs = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,43 +14,8 @@ public class FirstActivity extends AppCompatActivity {
         // Before this function, the screen is empty.
         setContentView(R.layout.activity_main);
 
-        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
-        String savedString = prefs.getString("ReserveName", "Enter something");
-        EditText typeField = findViewById(R.id.inputText);
-        typeField.setText(savedString);
-
-        Button saveButton = findViewById(R.id.saveButton);
-
-        saveButton.setOnClickListener( bt -> saveSharedPrefs( typeField.getText().toString()) );
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
+        ListView myList = findViewById(R.id.theListView);
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-
-    private void saveSharedPrefs(String stringToSave)
-    {
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("ReserveName", stringToSave);
-        editor.commit();
-    }
 }
