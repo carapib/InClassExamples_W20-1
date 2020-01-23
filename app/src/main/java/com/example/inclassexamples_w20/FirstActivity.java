f0eb93b6cd11489c16ded19564bfb56c701c6c7d
Eric Torunski <etorunski@gmail.com>
2020-01-23 12:26:39 -0500

    week 4 examples

--- app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/FirstActivity.java
@@ -2,9 +2,20 @@ package com.example.inclassexamples_w20;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FirstActivity extends AppCompatActivity {
+    private ArrayList<String> elements = new ArrayList<>( Arrays.asList( /*Empty*/ ) );
+    private MyListAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +26,30 @@ public class FirstActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView myList = findViewById(R.id.theListView);
-
+        myList.setAdapter( myAdapter = new MyListAdapter());
     }
 
+    private class MyListAdapter extends BaseAdapter{
+
+        public int getCount() { return elements.size();}
+
+        public Object getItem(int position) { return "This is row " + position; }
+
+        public long getItemId(int position) { return (long) position; }
+
+        public View getView(int position, View old, ViewGroup parent)
+        {
+            LayoutInflater inflater = getLayoutInflater();
+
+            //make a new row:
+            View newView = inflater.inflate(R.layout.row_layout, parent, false);
+
+            //set what the text should be for this row:
+            TextView tView = newView.findViewById(R.id.textGoesHere);
+            tView.setText( getItem(position).toString() );
+
+            //return it to be put in the table
+            return newView;
+        }
+    }
 }
