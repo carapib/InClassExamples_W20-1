270d284243e29765539ba513bc48be8320a876f8
Bert Carapiet <carapib@algonquincollege.com>
2020-03-02 20:34:27 -0500

    Updated with latest info

---
+++ app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.inclassexamples_w20;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyHTTPRequest req = new MyHTTPRequest();
+        req.execute("http://torunski.ca/CST2335.xml");  //Type 1
+    }
+                                                //Type1     Type2   Type3
+    private class MyHTTPRequest extends AsyncTask< String, Integer, String>
+    {
+              //Type3                Type1
+        public String doInBackground(String ... args)
+        {
+            publishProgress(25);
+            publishProgress(50);
+            publishProgress(75);
+            return "Done";
+
+        }
+
+                                    //Type 2
+        public void onProgressUpdate(Integer ... args)
+        {
+
+        }
+                                //Type3
+        public void onPostExecute(String fromDoInBackground)
+        {
+            Log.i("HTTP", fromDoInBackground);
+        }
+    }
+}
