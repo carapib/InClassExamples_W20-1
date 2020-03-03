f3731cb3567084791003c0f7ef8c385fd56e5a56
Bert Carapiet <carapib@algonquincollege.com>
2020-03-02 20:40:04 -0500

    Updated with http connection info

--- app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.inclassexamples_w20;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,27 +21,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MyHTTPRequest req = new MyHTTPRequest();
-        req.execute("http://torunski.ca/CST2335.xml");  //Type 1
+        req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
     }
-                                                //Type1     Type2   Type3
+
+
+    //Type1     Type2   Type3
     private class MyHTTPRequest extends AsyncTask< String, Integer, String>
     {
-              //Type3                Type1
+        //Type3                Type1
         public String doInBackground(String ... args)
         {
-            publishProgress(25);
-            publishProgress(50);
-            publishProgress(75);
-            return "Done";
+            try {
 
+                //create a URL object of what server to contact:
+                URL url = new URL(args[0]);
+
+                //open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //wait for data:
+                InputStream response = urlConnection.getInputStream();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return "Done";
         }
 
-                                    //Type 2
+        //Type 2
         public void onProgressUpdate(Integer ... args)
         {
 
         }
-                                //Type3
+        //Type3
         public void onPostExecute(String fromDoInBackground)
         {
             Log.i("HTTP", fromDoInBackground);
