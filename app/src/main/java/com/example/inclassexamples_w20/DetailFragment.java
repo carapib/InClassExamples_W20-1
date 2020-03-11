c28f998b186525f3e3e3128b4588e958d23dd6b1
Bert Carapiet <carapib@algonquincollege.com>
2020-03-11 08:37:22 -0400

    Updating with week7_fragmentLoading

--- app/src/main/java/com/example/inclassexamples_w20/DetailFragment.java
+++ app/src/main/java/com/example/inclassexamples_w20/DetailFragment.java
@@ -1,8 +1,7 @@
 package com.example.inclassexamples_w20;
 
 
-import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,17 +9,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 
 public class DetailFragment extends Fragment {
 
-    private boolean isTablet;
     private Bundle dataFromActivity;
     private long id;
-
-    public void setTablet(boolean tablet) { isTablet = tablet; }
-
+    private AppCompatActivity parentActivity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,29 +38,21 @@ public class DetailFragment extends Fragment {
         idView.setText("ID=" + id);
 
         // get the delete button, and add a click listener:
-        Button deleteButton = (Button)result.findViewById(R.id.deleteButton);
-        deleteButton.setOnClickListener( clk -> {
+        Button finishButton = (Button)result.findViewById(R.id.finishButton);
+        finishButton.setOnClickListener( clk -> {
 
-            if(isTablet) { //both the list and details are on the screen:
-                FragmentExample parent = (FragmentExample)getActivity();
-                parent.deleteMessageId((int)id); //this deletes the item and updates the list
+            //Tell the parent activity to remove
+                parentActivity.getSupportFragmentManager().beginTransaction().remove(this).commit();
+            });
 
-
-                //now remove the fragment since you deleted it from the database:
-                // this is the object to be removed, so remove(this):
-                parent.getSupportFragmentManager().beginTransaction().remove(this).commit();
-            }
-            //for Phone:
-            else //You are only looking at the details, you need to go back to the previous list page
-            {
-                EmptyActivity parent = (EmptyActivity) getActivity();
-                Intent backToFragmentExample = new Intent();
-                backToFragmentExample.putExtra(FragmentExample.ITEM_ID, dataFromActivity.getLong(FragmentExample.ITEM_ID ));
-
-                parent.setResult(Activity.RESULT_OK, backToFragmentExample); //send data back to FragmentExample in onActivityResult()
-                parent.finish(); //go back
-            }
-        });
         return result;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        //context will either be FragmentExample for a tablet, or EmptyActivity for phone
+        parentActivity = (AppCompatActivity)context;
     }
 }
--- app/src/main/java/com/example/inclassexamples_w20/FragmentExample.java
+++ app/src/main/java/com/example/inclassexamples_w20/FragmentExample.java
@@ -1,6 +1,7 @@
 package com.example.inclassexamples_w20;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -16,9 +17,7 @@ public class FragmentExample extends AppCompatActivity {
     public static final String ITEM_SELECTED = "ITEM";
     public static final String ITEM_POSITION = "POSITION";
     public static final String ITEM_ID = "ID";
-    public static final int EMPTY_ACTIVITY = 345;
 
-    ArrayAdapter<String> theAdapter;
 
     ArrayList<String> source = new ArrayList<>(Arrays.asList("One", "Two", "Three", "Four"));
 
@@ -30,12 +29,30 @@ public class FragmentExample extends AppCompatActivity {
         ListView theList = (ListView) findViewById(R.id.theList);
         boolean isTablet = findViewById(R.id.fragmentLocation) != null; //check if the FrameLayout is loaded
 
-        theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, source);
+        ArrayAdapter<String> theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, source);
         theList.setAdapter(theAdapter);
         theList.setOnItemClickListener((list, item, position, id) -> {
+            //Create a bundle to pass data to the new fragment
+            Bundle dataToPass = new Bundle();
+            dataToPass.putString(ITEM_SELECTED, source.get(position) );
+            dataToPass.putInt(ITEM_POSITION, position);
+            dataToPass.putLong(ITEM_ID, id);
 
-            //Add fragment loading here from slide 14.
-
+            if(isTablet)
+            {
+                DetailFragment dFragment = new DetailFragment(); //add a DetailFragment
+                dFragment.setArguments( dataToPass ); //pass it a bundle for information
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentLocation, dFragment) //Add the fragment in FrameLayout
+                        .commit(); //actually load the fragment.
+            }
+            else //isPhone
+            {
+                Intent nextActivity = new Intent(FragmentExample.this, EmptyActivity.class);
+                nextActivity.putExtras(dataToPass); //send data to next activity
+                startActivity(nextActivity); //make the transition
+            }
         });
     }
 }
